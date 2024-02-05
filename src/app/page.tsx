345d294f3894528db428e02a33dd2338843b1216
andreenakashima <andreenakashima@gmail.com>
2024-02-05 15:30:13 -0300

    Add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,34 @@
 import { Button } from "@/components/ui/button";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { LoginButton } from "@/components/auth/login-button";
+
+const font = Poppins({
+	subsets: ["latin"],
+	weight: ["600"],
+});
 
 export default function Home() {
 	return (
-		<div>
-			<p>Hello auth</p>
-			<Button>Click me</Button>
-		</div>
+		<main className="flex h-screen flex-col justify-center items-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+			<div className="space-y-6 text-center">
+				<h1
+					className={cn(
+						"text-6xl font-semibold text-white drop-shadow-md",
+						font.className
+					)}
+				>
+					Auth
+				</h1>
+				<p className="text-lg text-white">A simple authentication</p>
+				<div>
+					<LoginButton>
+						<Button variant="secondary" size="lg">
+							Sign In
+						</Button>
+					</LoginButton>
+				</div>
+			</div>
+		</main>
 	);
 }
