6870a1db06e043759281376c6ad5c12342c2c2e4
andreenakashima <andreenakashima@gmail.com>
2024-02-06 11:15:58 -0300

    Add register form

--- src/components/form-success.tsx
+++ src/components/form-success.tsx
@@ -8,7 +8,7 @@ export const FormSuccess = ({ message }: FormSuccessProps) => {
 	if (!message) return null;
 
 	return (
-		<div className="bg-emerald/15 p-3 flex rounded-md items-center text-sm text-emerald gap-x-2">
+		<div className="bg-emerald-500/15 p-3 flex rounded-md items-center text-sm text-emerald-500 gap-x-2">
 			<CheckCircledIcon className="w-4 h-4" />
 			<p>{message}</p>
 		</div>
